2eff45b1d7d8cb438ae83ec5d80f3edcb840b3df
mohammadrezakarimi <93293308+Satrap18@users.noreply.github.com>
2022-11-08 04:51:49 -0800

    Add files via upload

---
+++ code.py
@@ -0,0 +1,79 @@
+code = input("Mourse Code:")
+
+codes = code.split()
+
+for item in codes:
+    if item == '...':
+        print('S')
+    elif item == '---':
+        print('O')
+    elif item == '-':
+        print('T')
+    elif item == '-.':
+        print('N')
+    elif item == '.-':
+        print('A')
+    elif item == '.-.':
+        print('R')
+    elif item == '-...':
+        print('B')
+    elif item == '-.-.':
+        print('C')
+    elif item == '-..':
+        print('D')
+    elif item == '.':
+        print('E')
+    elif item == '..-.':
+        print('F')
+    elif item == '--.':
+        print('G')
+    elif item == '....':
+        print('H')
+    elif item == '..':
+        print('I')
+    elif item == '.---':
+        print('J')
+    elif item == '-.-':
+        print('K')
+    elif item == '.-..':
+        print('L')
+    elif item == '--':
+        print('M')
+    elif item == '.--.':
+        print('P')
+    elif item == '--.-':
+        print('Q')
+    elif item == '..-':
+        print('U')
+    elif item == '...-':
+        print('V')
+    elif item == '.--':
+        print('W')
+    elif item == '-..-':
+        print('X')
+    elif item == '-.--':
+        print('Y')
+    elif item == '--..':
+        print('Z')
+    elif item == '-----':
+        print('0')
+    elif item == '.----':
+        print('1')
+    elif item == '..---':
+        print('2')
+    elif item == '...--':
+        print('3')
+    elif item == '....-':
+        print('4')
+    elif item == '.....':
+        print('5')
+    elif item == '-....':
+        print('6')
+    elif item == '--...':
+        print('7')
+    elif item == '---..':
+        print('8')
+    elif item == '----.':
+        print('9')
+    elif item == '---...':
+        print(':')
